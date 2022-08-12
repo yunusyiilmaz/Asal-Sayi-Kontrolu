1689edfd50c85803dba520019f667d93fc3fd485
yunusyiilmaz <yns.ylmz1381@gmail.com>
2022-08-12 18:10:35 +0300

    Asal sayının kontrolü
    Asal sayının kontrolü

---
+++ ASAL SAYI KONTROLÜ/Asal sayı kontrolü/Asal sayı kontrolü/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Asal_sayı_kontrolü
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int kontrol = 0;
+            Console.Write("SAYI GİRİNİZ :");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < sayı; i++)
+            {
+                if (sayı % i == 0)
+                {
+                    kontrol++;
+                }
+            }
+            if (kontrol == 0)
+            {
+                Console.WriteLine("SAYI ASALDIR");
+            }
+            else
+            {
+                Console.WriteLine("SAYI ASAL DEĞİLDİR");
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
